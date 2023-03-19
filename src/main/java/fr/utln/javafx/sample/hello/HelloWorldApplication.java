5b35187fcbab962184a5e96cb1f60fa14891b238
rawi124 <rawia.ben.amira1997@gmail.com>
2023-03-19 23:39:06 +0100

    grille done

--- src/main/java/fr/utln/javafx/sample/hello/HelloWorldApplication.java
+++ src/main/java/fr/utln/javafx/sample/hello/HelloWorldApplication.java
@@ -36,7 +36,21 @@ public class HelloWorldApplication extends Application {
    private Scene scene = new Scene(disposition, 750, 550);
 
    TextField nom1 = new TextField();//le nom du premier utilisateur
+
    TextField nom2 = new TextField();//le nom du deuxieme utilisateur
+   TextField tailleGrille = new TextField();//la grille de taille n*n
+   final ColorPicker selectionneurCouleur1 = new ColorPicker();
+   final ColorPicker selectionneurCouleur2 = new ColorPicker();
+
+
+   public class Cell extends Pane{
+      public Cell(){
+         setStyle("-fx-border-color: black");
+         this.setPrefSize(300, 300);
+
+
+      }
+   }
 
    
    @Override
@@ -46,9 +60,9 @@ public class HelloWorldApplication extends Application {
       disposition.setPadding(new Insets(100, 100, 100, 100));
       disposition.setVgap(5);
       disposition.setHgap(5);
-      disposition.getChildren().addAll(nom1, nom2, btnDebut);
+      disposition.getChildren().addAll(nom1, nom2, btnDebut, tailleGrille, selectionneurCouleur1, selectionneurCouleur2);
 
-      GridPane.setConstraints(bienvenue, 0, 3);
+      GridPane.setConstraints(bienvenue, 0, 6);
       GridPane.setColumnSpan(bienvenue, 2);
       disposition.getChildren().add(bienvenue);
 
@@ -61,35 +75,70 @@ public class HelloWorldApplication extends Application {
       nom2.setPrefColumnCount(15);
       nom2.setPromptText("Joueue 2");
       GridPane.setConstraints(nom2, 0, 2);
-      GridPane.setConstraints(btnDebut, 1, 0);
+      GridPane.setConstraints(btnDebut, 0, 7);
 
+      //espace de declaration du TextField pour la taille de la grille
+      tailleGrille.setPrefColumnCount(20);
+      tailleGrille.setPromptText("taille de la grille");
+      GridPane.setConstraints(tailleGrille, 0, 3);
 
+      GridPane.setConstraints(selectionneurCouleur1, 0, 5);
+      GridPane.setConstraints(selectionneurCouleur2, 0, 4);
 
+      nom1.setStyle("    -fx-min-width: 40; -fx-min-height: 40;");
+      nom1.setFont(Font.font( 20));//pour la taille des caracteres
+
+      nom2.setStyle("    -fx-min-width: 40; -fx-min-height: 40;");
+      nom2.setFont(Font.font( 20));//pour la taille des caracteres
+
+      tailleGrille.setStyle("    -fx-min-width: 40; -fx-min-height: 40;");
+      tailleGrille.setFont(Font.font( 20));//pour la taille des caracteres
+
+      btnDebut.setStyle("-fx-min-width: 100; -fx-max-height: 100;");
       btnDebut.setOnAction(new EventHandler<ActionEvent>() {
-
          @Override
          public void handle(ActionEvent e) {
-            if ((nom1.getText() != null && nom2.getText() != null)) {
-               if (nom2.getText().equals(nom1.getText())) {
-                  bienvenue.setText(
-                          "veuillez saisir deux noms differents !");
-               } else {
-                  bienvenue.setText("vous pouvez commencer la partie !");
-                  GridPane jeu = new GridPane();
-                  Scene scene2 = new Scene(jeu, 600, 400);
-                  stagePrincipale.setScene(scene2);
+            if (!nom1.getText().equals("")) {
+               if (!nom2.getText().equals("")) {
+                  if (!tailleGrille.getText().equals("")) {
+                     if (nom2.getText().equals(nom1.getText())) {
+                        bienvenue.setText("veuillez saisir deux noms differents !");
 
+                     } else {
+                        bienvenue.setText("vous pouvez commencer la partie !");
+                        selectionneurCouleur1.getValue();
+                        selectionneurCouleur2.getValue();
+                        GridPane jeu = new GridPane();
+                        Scene scene2 = new Scene(jeu, 600, 400);
+                        int n = Integer.parseInt(tailleGrille.getText());
+                        Cell[][] grille = new Cell[n][n];
+                        for(int i = 0; i < n;i++){
+                           for(int j = 0 ; j < n ;j ++)
+                           {
+                              grille[i][j] = new Cell();
+                              jeu.add(grille[i][j], j, i);
+                           }
+                        }
+                        stagePrincipale.setScene(scene2);
+                     }
+                  } else {
+                     bienvenue.setText("veuillez saisir la taille de la grille! ");
+                  }
+               } else {
+                  bienvenue.setText("veuillez saisir le nom du second joueur");
                }
-            } else {
-               bienvenue.setText("veuillez remplir les champs !");
+
 
             }
-         }
+            else{
+                     bienvenue.setText("veuillez saisir le nom du premier joueur");
+                  }
+            }});
 
-      });
 
-
-
+      disposition.setStyle("-fx-background-color: #9E9E9E");
+      bienvenue.setFont(Font.font( 25));//pour la taille des caracteres
+      bienvenue.setTextFill(Color.WHITESMOKE);//la couleur du texte
       stagePrincipale.setTitle("MORPION");
       stagePrincipale.setScene(scene);
 
